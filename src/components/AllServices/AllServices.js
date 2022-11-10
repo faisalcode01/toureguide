7ce939f8ccb996e0780fbdb9d5cc6a65c4b86666
faisalcode01 <faisal.code01@gmail.com>
2022-11-10 22:04:47 +0600

    all services heading change

--- src/components/AllServices/AllServices.js
+++ src/components/AllServices/AllServices.js
@@ -14,17 +14,15 @@ const AllServices = () => {
       return (
   <div className="bg-white">
     <div className="space-y-16 container xl:max-w-7xl mx-auto px-4 py-16 lg:px-8 lg:py-32">
-      <div className="text-center">
-        <div className="text-sm uppercase font-bold tracking-wider mb-1 text-indigo-700">
-          Amazing Team
-        </div>
-        <h2 className="text-3xl md:text-4xl font-extrabold mb-4">
-          The people under the hood
-        </h2>
-        <h3 className="text-lg md:text-xl md:leading-relaxed font-medium text-gray-600 lg:w-2/3 mx-auto">
-          They are working nonstop behind the scenes to help you build better products, web services and websites.
-        </h3>
-      </div>
+    <div className="text-center">
+
+      <h2 className="text-3xl md:text-4xl font-extrabold mb-4">
+      ALL DESTINATIONS
+      </h2>
+      <h3 className="text-lg md:text-xl md:leading-relaxed font-medium text-gray-600 lg:w-2/3 mx-auto">
+      Looking for your next great adventure? We can help. Here is a sampling of our most popular destinations. Experience private tours with authentic local flavour.
+      </h3>
+    </div>
   
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 md:gap-16">
       <PhotoProvider>
